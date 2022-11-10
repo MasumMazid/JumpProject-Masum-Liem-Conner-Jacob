c2370f054fe951fc8a6735139511f0aed80e8f29
jacob-cantrell <117400722+jacob-cantrell@users.noreply.github.com>
2022-11-10 11:34:55 -0500

    Update TvShow.java

--- JumpProject/src/main/java/models/TvShow.java
+++ JumpProject/src/main/java/models/TvShow.java
@@ -1,4 +1,32 @@
 package models;
 
 public class TvShow {
+   private int tvShowId;
+   private String name;
+
+   public TvShow(int tvShowId, String name) {
+       this.tvShowId = tvShowId;
+       this.name = name;
+   }
+
+   public int getTvShowID() {
+       return this.tvShowId;
+   }
+
+   public void setTvShowId(int id) {
+       this.tvShowId = id;
+   }
+
+   public String getName() {
+       return this.name;
+   }
+
+   public void setName(String name) {
+       this.name = name;
+   }
+
+   @Override
+   public String toString() {
+       return "TvShow [tvShowId=" + tvShowId + ", name=" + name + "]";
+   }
 }
