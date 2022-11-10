17699d5a13bf09d4c7bfc59f9eff6a87d842d482
jacob-cantrell <117400722+jacob-cantrell@users.noreply.github.com>
2022-11-10 16:45:47 -0500

    Update and rename tvShow.java to TvShow.java

---
+++ src/main/java/daos/TvShow.java
@@ -0,0 +1,10 @@
+package daos;
+
+import java.util.List;
+import main.java.models.TvShow;
+
+public interface TvShowDAO {
+    public List<TvShow> getAllTvShows();
+    public TvShow getTvShowById(int id);
+    public TvShow getTvShowByName(String name);
+}
