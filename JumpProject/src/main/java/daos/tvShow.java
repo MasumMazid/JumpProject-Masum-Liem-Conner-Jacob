09f5accd4cd38f1e21bb76164d313d6a2a460bf0
jacob-cantrell <117400722+jacob-cantrell@users.noreply.github.com>
2022-11-10 11:32:23 -0500

    Update tvShow.java

--- JumpProject/src/main/java/daos/tvShow.java
+++ JumpProject/src/main/java/daos/tvShow.java
@@ -1,4 +1,29 @@
 package daos;
 
 public class tvShow {
+   public tvShow(int tvShowId, String name) {
+       this.tvShowId = tvShowId;
+       this.name = name;
+   }
+
+   public int getTvShowID() {
+       return this.tvShowId;
+   }
+
+   public void setTvShowId(int id) {
+       this.tvShowId = id;
+   }
+
+   public String getName() {
+       return this.name;
+   }
+
+   public void setName(String name) {
+       this.name = name;
+   }
+
+   @Override
+   public String toString() {
+       return "TvShow [tvShowId=" + tvShowId + ", name=" + name + "]";
+   }
 }
