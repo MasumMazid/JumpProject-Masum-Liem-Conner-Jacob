dda794410ed80b4e7bd1ef622b012e9532f1e49c
jacob-cantrell <117400722+jacob-cantrell@users.noreply.github.com>
2022-11-10 16:46:48 -0500

    Update and rename userDao.java to UserDao.java

---
+++ src/main/java/daos/UserDao.java
@@ -0,0 +1,21 @@
+package daos;
+
+import java.util.List;
+import main.java.models.User;
+
+public interface UserDAO {
+    public List<User> getAllUsers();
+    public User getUserById(int userId);
+    public User getUserByUsername(String name);
+    public List<String> getShowsCompleted(int id);
+    public List<String> getShowsInProgress(int id);
+    public List<String> getShowsNotCompleted(int id);
+    public boolean addUser(User user);
+    public boolean deleteUser(int id);
+    public boolean updateUserPassword(User user);
+    public boolean updateUserProgress(User user, int tv_id, int progress);
+    public boolean updateUserProgress(User user, String show_name, int progress);
+    public int getNumCompleted(User user);
+    public int getNumInProgress(User user);
+    public int getNumNotCompleted(User user);
+}
