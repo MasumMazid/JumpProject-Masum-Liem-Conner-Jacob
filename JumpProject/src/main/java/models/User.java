e64a1e77b1163af75eb0a2e0d680e76e4890fdc5
jacob-cantrell <117400722+jacob-cantrell@users.noreply.github.com>
2022-11-11 10:28:44 -0500

    Update User.java

--- JumpProject/src/main/java/models/User.java
+++ JumpProject/src/main/java/models/User.java
@@ -1,32 +1,32 @@
 package models;
 
 public class User {
-    private String user_id;
+    private int userId;
     private String user_name;
     private String user_password;
     private String user_userName;
 
-    public String getUser_id() {
-        return user_id;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public String getUser_name() {
+    public String getUserName() {
         return user_name;
     }
 
-    public void setUser_name(String user_name) {
+    public void setUserName(String user_name) {
         this.user_name = user_name;
     }
 
-    public String getUser_password() {
+    public String getUserPassword() {
         return user_password;
     }
 
-    public void setUser_password(String user_password) {
+    public void setUserPassword(String user_password) {
         this.user_password = user_password;
     }
 
@@ -38,8 +38,8 @@ public class User {
         this.user_userName = user_userName;
     }
 
-    public User(String user_id, String user_name, String user_password, String user_userName) {
-        this.user_id = user_id;
+    public User(int userId, String user_name, String user_password, String user_userName) {
+        this.userId = userId;
         this.user_name = user_name;
         this.user_password = user_password;
         this.user_userName = user_userName;
@@ -48,7 +48,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "user_id='" + user_id + '\'' +
+                "user_id='" + userId + '\'' +
                 ", user_name='" + user_name + '\'' +
                 ", user_password='" + user_password + '\'' +
                 ", user_userName='" + user_userName + '\'' +
